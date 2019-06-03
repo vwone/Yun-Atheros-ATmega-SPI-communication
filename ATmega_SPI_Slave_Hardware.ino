946c40e0b3ab84bb63451aeb09c555ebdc19236a
vwone <vpw@neuf.fr>
2019-06-03 21:40:59 +0200

    improved functionalities
    Adds SPI frame management
    gets quicker using SPI interrupts on the Arduino side

--- ATmega_SPI_Slave_Hardware.ino
+++ ATmega_SPI_Slave_Hardware.ino
@@ -2,10 +2,15 @@
 // Upload the sketch
 // Open the monitor
 
-//#define DEBUG
+#define DEBUG
 
-char Received;
-char count=0x1;
+#define SPI_FRAME_SIZE 12
+char SPI_Received[SPI_FRAME_SIZE];
+char SPI_Send[SPI_FRAME_SIZE];
+char SPI_Count;
+
+char Count;
+
 
 void setup (void)
 {
@@ -36,44 +41,103 @@ void setup (void)
   // CPHA                                : 0
   // SPR1                                : x
   // SPR0 : fosc/4 = 16 Mhz/4 = 4 Mhz    : x  //No effect on slave mode
-  SPCR = 0x40; // Interrupt disable
-  //SPCR = 0xC0; Interrupt enable
+  //SPCR = 0x40; // Interrupt disable
+  SPI_Count = 0;
+  SPCR = 0xC0; // Interrupt enable
 
   // SPSR
   // SPIF : SPI Interrupt Flag : Reset : 0
   // SPI2X : fosc/4 = 16 Mhz/4 = 4 Mhz   : x //No effect on slave mode, but Master musn't exceed this speed. 
   SPSR = 0;
+
+  Count = 0;
   
 }  // end of setup
-
 
 // SPI interrupt routine
 ISR (SPI_STC_vect)
 {
-  Serial.print( "Received :");    
-  Serial.println( Received, HEX);
+  //Send
+  SPDR = SPI_Send[SPI_Count+1];
+
+  //Receive
+  SPI_Received[SPI_Count] = SPDR;
+
+  SPI_Count++;
+  
 }  // end of interrupt routine SPI_STC_vect
+
 
 // main loop - wait for flag set in interrupt routine
 void loop (void)
 {
-  //Increment return value
-  SPDR = count++;
-  
+
+  if( SPI_Count == SPI_FRAME_SIZE)
+  {
+#ifdef DEBUG  
+    Serial.print( "Received : 0x");    
+    for( SPI_Count=0; SPI_Count < SPI_FRAME_SIZE; SPI_Count++)
+    {
+      Serial.print( SPI_Received[SPI_Count] & 0xFF, HEX);    
+    }      
+    Serial.println( "" );    
+#endif //DEBUG
+
+    if( SPI_Received[0] == 'C')
+    {
+      Count = 0;   
+    }
+
+    for( SPI_Count=0; SPI_Count < SPI_FRAME_SIZE; SPI_Count++)
+    {
+      SPI_Send[SPI_Count] = Count++;
+    }
+    
+#ifdef DEBUG  
+    Serial.print( "Send : 0x");    
+    for( SPI_Count=0; SPI_Count < SPI_FRAME_SIZE; SPI_Count++)
+    {
+      Serial.print( SPI_Send[SPI_Count] & 0xFF, HEX);    
+    }      
+    Serial.println( "" );    
+#endif //DEBUG
+
+    //prepare the next package
+    SPI_Count = 0;
+    SPDR = SPI_Send[SPI_Count];
+ 
+  }
+
+
+
+//  SPDR = 0x55; 
   // Wait for transmission complete
   // reset when read at '1'
-  while( !(SPSR & (1<<SPIF) ));
+//  while( !(SPSR & (1<<SPIF) ));
 
-  Received = SPDR;
-
-  if(Received == 'C')
-  {
-    count = 0;    
-  }
-  
-#ifdef DEBUG  
-  Serial.print( "Received : 0x");    
-  Serial.println( Received & 0xFF, HEX );    
-#endif //DEBUG
-    
+      
 } // end of loop
+
+void InitialiseInterrupt(){
+
+  //Turn off global interrupt
+  cli();    // switch interrupts off while messing with their settings
+//noInterrupts  //  Do the same (To be analysed)              
+
+//  EICRA = 0x2;          // Falling edge of INT0 generates an interrupt request
+//  EICRB = 0xFF;          // Any edge of INT0 generates an interrupt request
+  EIMSK = 0;          // Disable INTx interrupt enbale
+  PCICR = 1;          // PCIE0 = 1 : Enable PCINT0 interrupt
+  PCIFR = 1;          // PCIF0 = 1 : Clear interrupt flags PCIFR 
+
+//  PCMSK0 = 0x80;         //Enable PCINT7 only !!!   
+//  PCMSK0 = 0x82;         //Enable PCINT7 & PCINT1 only !!!   
+  PCMSK0 = 0x88;         //Enable PCINT7 & PCINT3 only !!!   
+//  PCMSK0 = 0x84;         //Enable PCINT7 & PCINT2 only !!!   
+
+////  PCMSK1 = 0x2;         //PCINT1 15:8 on other board but Yun!!!
+  
+  
+  sei();    // turn interrupts back on (also useable interrupts())
+  
+}//InitialiseInterrupt()
--- Atheros_SPI_Master.c
+++ Atheros_SPI_Master.c
@@ -5,6 +5,7 @@
 #         it isn't automatically assereted. 
 #
 echo 26 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio26/direction
 */
 #include <stdint.h>
 #include <stdio.h>
@@ -21,22 +22,37 @@ echo 26 > /sys/class/gpio/export
 
 // Level shifters output enable = GPIO_21
 #define LEVEL_SHIFTER_ENABLE "/sys/class/gpio/gpio21/value"
+#define GPIO_21 21
 
 // Slave Select pin = GPIO_26
 #define SLAVE_SELECT "/sys/class/gpio/gpio26/value"
+#define GPIO_26 26
 
-static const char *device = "/dev/spidev1.0";
+static const char *device = "/dev/spidev1.0"; // LEDE Distribution
+//static const char *device = "/dev/ttySPI0"; // Linino distribution
 static uint8_t mode = 0;
 static uint8_t bits = 8;
 //Atmega at 16 Mhz => 4 Mhz is the max speed.
-static uint32_t speed = 4000000;
-static uint16_t delay;
+//static uint32_t speed = 4000000;
+static uint32_t speed = 100000;
+//static uint16_t delay = 1000; // works fine with cs_change = 0 or 1
+//static uint16_t delay = 100; // works fine with cs_change = 0 or 1
+static uint16_t delay = 0; // works fine with cs_change = 0 or 1
 
 static void pabort(const char *s);
-static int Set_GPIO(char Path[], char Value[]);
-static int transfer(int fd, struct spi_ioc_transfer *transfer_stuct, unsigned char *Pt_Data);
 
-unsigned char send[2], received[2];
+int Export_GPIO(unsigned int GPIO_Num, char *Direction);
+static int Set_GPIO(char Path[], char Value[]);
+
+static int transfer(int fd, struct spi_ioc_transfer *transfer_stuct, unsigned char *Pt_Data);
+static int transfer2(int fd, struct spi_ioc_transfer *transfer_stuct);
+
+//'C' Clear arduino counter at first emission
+unsigned char Send_Buf[13]={'C',0,0,0,0,0,0,0,0,0,0,0,0};
+unsigned char Receive_Buf[13]={0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+unsigned char Receive[13]={0,0,0,0,0,0,0,0,0,0,0,0,0};
+
 unsigned char Data;
 unsigned char n;
 
@@ -46,16 +62,40 @@ int main (int argc, char* argv[])
 	int fd;
 
 	//ioctl structure
+	
+	struct spi_ioc_transfer transfer_stuct_tab[12];
+
+	for (n = 0; n < 12 ; n++)
+	{
+		memset(&transfer_stuct_tab[n], 0, sizeof(transfer_stuct_tab[n]));
+		transfer_stuct_tab[n].tx_buf = (unsigned long) &Send_Buf[n];
+		transfer_stuct_tab[n].rx_buf = (unsigned long) &Receive_Buf[n];
+		transfer_stuct_tab[n].len = 1;
+		transfer_stuct_tab[n].delay_usecs = delay;
+//		transfer_stuct_tab[n].cs_change = 0;
+		transfer_stuct_tab[n].cs_change = 1;
+		transfer_stuct_tab[n].speed_hz = speed;
+		transfer_stuct_tab[n].bits_per_word = bits;
+
+	}
+
+	
 	struct spi_ioc_transfer transfer_stuct = 
 	{
-		.tx_buf = (unsigned long) send,
-		.rx_buf = (unsigned long) received,
+		.tx_buf = (unsigned long) Send_Buf,
+		.rx_buf = (unsigned long) Receive_Buf,
 		.len = 1,
-		// .delay_usecs = delay,
+		.delay_usecs = delay,
+		.cs_change = 1,
 		.speed_hz = speed,
 		.bits_per_word = bits,
 	};
-		
+	
+	// Export GPIO
+	Export_GPIO(GPIO_26, "out" );
+	//Fails due to the absence of the "direction" file in the gpio21 directory ?!
+	//Export_GPIO(GPIO_21, "out" );
+	
 	// Output disable
 	Set_GPIO(LEVEL_SHIFTER_ENABLE, "1");
 
@@ -99,7 +139,7 @@ int main (int argc, char* argv[])
 	printf("spi mode: %d\n", mode);
 	printf("bits per word: %d\n", bits);
 	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
-
+/*
 	//Clear AVR Counter
 	Data = 'C';
 	
@@ -143,6 +183,7 @@ int main (int argc, char* argv[])
 		{
 			printf("Counter Error!!! : n 0x%x : Data : 0x%x\n", n, Data);
 		}
+
 #ifdef DEBUG		
 		else
 		{
@@ -156,7 +197,70 @@ int main (int argc, char* argv[])
 
 	n--;
 	printf("Counter loop : n 0x%x : Data : 0x%x\n", n, Data);			
+*/
+/*
+	
+	for(n=0; n<12; n++)
+	{
+		Data = 'C';
+		
+		if( transfer( fd, &transfer_stuct, &Data) < 0)
+		{
+			pabort("Transfert failed !!!");
+		}
+		
+		Receive[n] = Data;
+	}
 
+	printf("Received : 0x%x%x%x%x 0x%x%x%x%x 0x%x%x%x%x\n",
+		Receive[0],
+		Receive[1],
+		Receive[2],
+		Receive[3],
+		Receive[4],
+		Receive[5],
+		Receive[6],
+		Receive[7],
+		Receive[8],
+		Receive[9],
+		Receive[10],
+		Receive[11]);
+*/
+/*
+	if( transfer2( fd, &transfer_stuct[0]) < 0)
+	{
+		pabort("Transfer failed !!!");
+	}
+*/	
+	// Slave asserted
+   	Set_GPIO(SLAVE_SELECT, "0");
+
+//   if (ioctl(fd, SPI_IOC_MESSAGE(1), &transfer_stuct)<0)
+//   if (ioctl(fd, SPI_IOC_MESSAGE(12), &transfer_stuct)<0)
+   if (ioctl(fd, SPI_IOC_MESSAGE(12), &transfer_stuct_tab[0])<0 )
+   {
+      perror("Failed to send SPI message");
+	  return -1;
+   }
+   
+	// Slave negated
+  	Set_GPIO(SLAVE_SELECT, "1");
+
+	printf("Received : 0x%x%x%x%x 0x%x%x%x%x 0x%x%x%x%x\n",
+		Receive_Buf[0],
+		Receive_Buf[1],
+		Receive_Buf[2],
+		Receive_Buf[3],
+		Receive_Buf[4],
+		Receive_Buf[5],
+		Receive_Buf[6],
+		Receive_Buf[7],
+		Receive_Buf[8],
+		Receive_Buf[9],
+		Receive_Buf[10],
+		Receive_Buf[11]);
+
+	
 	close(fd);
 
 	// Output disable
@@ -164,17 +268,18 @@ int main (int argc, char* argv[])
 	
 	return 0;
 	
-}
+}//main()
 
 static int transfer(int fd, struct spi_ioc_transfer *transfer_stuct, unsigned char *Pt_Data)
 {
 	/* Transfer type 1*/
-   send[0]= *Pt_Data;
+   Send_Buf[0]= *Pt_Data;
 
 	// Slave asserted
    	Set_GPIO(SLAVE_SELECT, "0");
 
-   if (ioctl(fd, SPI_IOC_MESSAGE(1), transfer_stuct)<0){
+//   if (ioctl(fd, SPI_IOC_MESSAGE(1), transfer_stuct)<0){
+   if (ioctl(fd, SPI_IOC_MESSAGE(12), transfer_stuct)<0){
       perror("Failed to send SPI message");
 	  return -1;
    }
@@ -182,8 +287,27 @@ static int transfer(int fd, struct spi_ioc_transfer *transfer_stuct, unsigned ch
 	// Slave negated
   	Set_GPIO(SLAVE_SELECT, "1");
 	
-	*Pt_Data = received[0];
+	*Pt_Data = Receive_Buf[0];
 	
+	return 0;
+	
+}//transfer()
+
+static int transfer2(int fd, struct spi_ioc_transfer *transfer_stuct)
+{
+
+	// Slave asserted
+   	Set_GPIO(SLAVE_SELECT, "0");
+
+//   if (ioctl(fd, SPI_IOC_MESSAGE(1), transfer_stuct)<0){
+   if (ioctl(fd, SPI_IOC_MESSAGE(12), transfer_stuct)<0){
+      perror("Failed to send SPI message");
+	  return -1;
+   }
+   
+	// Slave negated
+  	Set_GPIO(SLAVE_SELECT, "1");
+		
 	return 0;
 	
 }//transfer()
@@ -200,7 +324,63 @@ int Set_GPIO(char Path[], char Value[])
 	
 	return 0;
 	
-}//Write_GPIO()
+}//Set_GPIO()
+
+int Export_GPIO(unsigned int GPIO_Num, char *Direction)
+{
+	FILE *fp;
+	char Path[255];
+
+	//Export ?
+	sprintf( Path, "/sys/class/gpio/gpio%u", GPIO_Num);
+	printf("Exporting : %s\n", Path);
+
+	//File exits?
+//	if(!fileexists(Path))
+//	{
+//		fp = fopen("/sys/class/gpio/export", "w+");	
+//		fprintf( fp, "%u", GPIO_Num);		
+//		fclose(fp);		
+//	}
+
+	if( (fp = fopen(Path, "w")) == NULL )
+	{
+		printf("File doesn't exist : %s\n", Path);
+		fp = fopen("/sys/class/gpio/export", "w");	
+		if (fp != NULL)
+		{
+			printf("/sys/class/gpio/export open\n", GPIO_Num);			
+		}
+		else
+		{
+			printf("Failed to open : /sys/class/gpio/export\n", GPIO_Num);						
+		}
+		
+		printf("/sys/class/gpio/export > %u\n", GPIO_Num);
+		fprintf( fp, "%u\n", GPIO_Num);		
+		fclose(fp);		
+	}
+	else
+	{
+		printf("File exists : %s\n", Path);
+		fclose(fp);
+	}
+	
+	//Set Direction
+	sprintf( Path, "/sys/class/gpio/gpio%u/direction", GPIO_Num);
+	printf("Setting direction : %s as %s\n", Path, Direction);
+
+	fp = fopen(Path, "w+");	
+	printf(Direction);
+	fprintf( fp, "%s\n", Direction);		
+	fclose(fp);		
+	
+	return 0;
+	
+}//Export_GPIO()
+
+#define LEVEL_SHIFTER_ENABLE "/sys/class/gpio/gpio21/value"
+
 
 static void pabort(const char *s)
 {
